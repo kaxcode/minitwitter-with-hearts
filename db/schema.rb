f5d89a77708755e7fa4844f4386bcaa36f8adb0c
Kenia Valladarez <keniavalladarez@gmail.com>
2016-12-13 15:29:47 -0500

    Added a vote model to count likes/hearts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212183113) do
+ActiveRecord::Schema.define(version: 20161213202918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 20161212183113) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.boolean  "upvote"
+    t.integer  "user_id"
+    t.integer  "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tweet_id"], name: "index_votes_on_tweet_id", using: :btree
+  end
+
+  add_foreign_key "votes", "tweets"
 end
